5d8a7048ce392d8246d442abc24fb248764c5554
Max Minh Luan Nguyen <maxminhluannguyen@maxs-mbp.mynetworksettings.com>
2026-07-08 14:31:26 -0400

    UI polishing: enlarged FireIcon and converted it to a custom multi-colored pixel art fire sprite

--- components/icons/FireIcon.tsx
+++ components/icons/FireIcon.tsx
@@ -4,31 +4,59 @@ interface FireIconProps {
     className?: string;
 }
 
-const FireIcon: React.FC<FireIconProps> = ({ className = "h-4 w-4 inline-block align-middle" }) => (
+const FireIcon: React.FC<FireIconProps> = ({ className = "h-6 w-6 inline-block align-middle" }) => (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className={className} 
-        viewBox="0 0 24 24" 
+        viewBox="0 0 12 12" 
+        shapeRendering="crispEdges"
         fill="currentColor"
     >
-        <defs>
-            <linearGradient id="fireGradient" x1="0%" y1="100%" x2="0%" y2="0%">
-                <stop offset="0%" stopColor="#ef4444" /> {/* red-500 */}
-                <stop offset="40%" stopColor="#f97316" /> {/* orange-500 */}
-                <stop offset="100%" stopColor="#fbbf24" /> {/* amber-400 */}
-            </linearGradient>
-        </defs>
-        {/* Outer Flame */}
-        <path 
-            fill="url(#fireGradient)"
-            d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z" 
-        />
-        {/* Inner Flame (Yellow Core) */}
-        <path 
-            fill="#fef08a" 
-            opacity={0.85}
-            d="M12 17c.8 0 1.5-.7 1.5-1.5 0-.8-.3-1.2-.6-1.8-.6-1.2-.1-2.4 1.1-3.6.3 1.5 1.2 3 2 4 .6.6.9 1.2.9 2a3.5 3.5 0 1 1-7 0c0-.7.2-1.4.5-1.8.3-.3.8-.5 1.1-.3.3.2.5.5.5.8 0 .8.2 1.4.5 1.6.3.3.7.5 1 .5z" 
-        />
+        {/* Red Pixels */}
+        <g fill="#ff3b30">
+            <rect x="5" y="1" width="1" height="1" />
+            <rect x="4" y="2" width="2" height="1" />
+            <rect x="7" y="2" width="1" height="1" />
+            <rect x="3" y="3" width="1" height="1" />
+            <rect x="6" y="3" width="3" height="1" />
+            <rect x="2" y="4" width="1" height="1" />
+            <rect x="8" y="4" width="2" height="1" />
+            <rect x="1" y="5" width="2" height="1" />
+            <rect x="9" y="5" width="2" height="1" />
+            <rect x="1" y="6" width="1" height="1" />
+            <rect x="10" y="6" width="1" height="1" />
+            <rect x="0" y="7" width="2" height="1" />
+            <rect x="10" y="7" width="2" height="1" />
+            <rect x="0" y="8" width="1" height="1" />
+            <rect x="11" y="8" width="1" height="1" />
+            <rect x="0" y="9" width="2" height="1" />
+            <rect x="10" y="9" width="2" height="1" />
+            <rect x="1" y="10" width="3" height="1" />
+            <rect x="8" y="10" width="3" height="1" />
+            <rect x="3" y="11" width="6" height="1" />
+        </g>
+        {/* Orange Pixels */}
+        <g fill="#ff9500">
+            <rect x="4" y="3" width="2" height="1" />
+            <rect x="3" y="4" width="1" height="1" />
+            <rect x="6" y="4" width="2" height="1" />
+            <rect x="3" y="5" width="1" height="1" />
+            <rect x="8" y="5" width="1" height="1" />
+            <rect x="2" y="6" width="2" height="1" />
+            <rect x="8" y="6" width="2" height="1" />
+            <rect x="2" y="7" width="3" height="1" />
+            <rect x="7" y="7" width="3" height="1" />
+            <rect x="1" y="8" width="10" height="1" />
+            <rect x="2" y="9" width="8" height="1" />
+            <rect x="4" y="10" width="4" height="1" />
+        </g>
+        {/* Yellow Pixels */}
+        <g fill="#ffcc00">
+            <rect x="4" y="4" width="2" height="1" />
+            <rect x="4" y="5" width="4" height="1" />
+            <rect x="4" y="6" width="4" height="1" />
+            <rect x="5" y="7" width="2" height="1" />
+        </g>
     </svg>
 );
 
